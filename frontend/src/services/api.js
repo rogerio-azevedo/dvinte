c4aaffe26e5bf7e45d4b0aead63dacc4e6757e40
Rogerio <rogerio.francis@gmail.com>
2020-05-02 01:23:45 -0400

    Frontend Ajuste API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:4800',
-  baseURL: '',
-  // baseURL: 'https://cors-anywhere.herokuapp.com/http://167.71.255.4',
+  // baseURL: '167.71.255.4',
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://198.211.112.112',
 })
 
 export default api
