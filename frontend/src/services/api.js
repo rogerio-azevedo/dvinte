589cbc2c3d380228da10c6556e81fb6565071aa1
Rogerio <rogerio.francis@gmail.com>
2020-05-02 10:23:13 -0400

    Frontend Ajuste Redirects

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:4800',
-  // baseURL: '167.71.255.4',
+  // baseURL: '198.211.112.112',
   baseURL: 'https://cors-anywhere.herokuapp.com/http://198.211.112.112',
 })
 
