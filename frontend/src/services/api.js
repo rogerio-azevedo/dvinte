4d0d99c307c50a8f3e6acbc7b8df4a30619a0f75
Rogerio <rogerio.francis@gmail.com>
2020-05-03 13:57:19 -0400

    Frontend - Add https

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'http://localhost:4800',
-  baseURL: 'http://api.dvinte.com.br',
+  // yarnbaseURL: 'http://localhost:4800',
+  baseURL: 'https://api.dvinte.com.br',
   // baseURL: 'https://cors-anywhere.herokuapp.com/http://198.211.112.112',
 })
 
