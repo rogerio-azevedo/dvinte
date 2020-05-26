3d7bf0def8a6a0d7a31adadcf9a576ed7ccc269c
Rogerio <rogerio.francis@gmail.com>
2020-05-26 15:55:09 -0400

    SetClass arrays

--- frontend/src/components/CharClass/index.js
+++ frontend/src/components/CharClass/index.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 import { Container, ClassInput, ClassValueInput } from './styles'
 
-export default function CharClass({ classes, loading }) {
-  const [newClass, setNewClass] = useState()
+export default function CharClass({ classes }) {
+  const [newClass, setNewClass] = useState({})
+  const [newClass2, setNewClass2] = useState({})
+  const [newClass3, setNewClass3] = useState({})
+  const [newClass4, setNewClass4] = useState({})
 
   async function loadChass() {
     setNewClass(classes && classes[0])
+    setNewClass2(classes && classes[1])
+    setNewClass3(classes && classes[2])
+    setNewClass4(classes && classes[3])
   }
 
   useEffect(() => {
@@ -20,19 +27,23 @@ export default function CharClass({ classes, loading }) {
           <ClassInput defaultValue={newClass && newClass.name} />
           <ClassValueInput defaultValue={newClass && newClass.level} />
         </li>
-        {/* <li>
-          <ClassInput defaultValue={len2.name} />
-          <ClassValueInput defaultValue={len2.level} />
+        <li>
+          <ClassInput defaultValue={newClass2 && newClass2.name} />
+          <ClassValueInput defaultValue={newClass2 && newClass2.level} />
         </li>
         <li>
-          <ClassInput defaultValue={len3.name} />
-          <ClassValueInput defaultValue={len3.level} />
+          <ClassInput defaultValue={newClass3 && newClass3.name} />
+          <ClassValueInput defaultValue={newClass3 && newClass3.level} />
         </li>
         <li>
-          <ClassInput defaultValue={len4.name} />
-          <ClassValueInput defaultValue={len4.level} />
-        </li> */}
+          <ClassInput defaultValue={newClass4 && newClass4.name} />
+          <ClassValueInput defaultValue={newClass4 && newClass4.level} />
+        </li>
       </ul>
     </Container>
   )
+}
+
+CharClass.propTypes = {
+  classes: PropTypes.func.isRequired,
 }
