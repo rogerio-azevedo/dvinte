f14f2859319dead085de386a70ca57f48a1b8751
Rogerio <rogerio.francis@gmail.com>
2020-05-26 19:26:41 -0400

    !load on component

--- frontend/src/components/CharClass/index.js
+++ frontend/src/components/CharClass/index.js
@@ -1,38 +1,68 @@
 import React, { useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { object } from 'prop-types'
 
 import { Container, ClassInput, ClassValueInput } from './styles'
 
-export default function CharClass({ classes, loading }) {
-  const [classArray, setClassArray] = useState(classes)
+export default function CharClass({ classes }) {
+  const [load, setLoad] = useState(true)
+  const [classArray, setClassArray] = useState([])
 
-  // async function loadClass() {
-  //   setClassArray(classes)
-  // }
-  // useEffect(() => {
-  //   loadClass()
-  // }, []) // eslint-disable-line
+  async function loadClass() {
+    setClassArray(classes)
+  }
+  useEffect(() => {
+    loadClass()
+    setLoad(false)
+  }, []) // eslint-disable-line
 
   return (
     <Container>
       <ul>
         <li>
-          <ClassInput defaultValue={classArray && classArray[0].name} />
-          <ClassValueInput defaultValue={classArray && classArray[0].level} />
+          {!load && (
+            <ClassInput defaultValue={classArray && classArray[0].name} />
+          )}
+          {!load && (
+            <ClassValueInput
+              defaultValue={!load && classArray && classArray[0].level}
+            />
+          )}
         </li>
         <li>
-          <ClassInput defaultValue={classArray && classArray[1].name} />
-          <ClassValueInput defaultValue={classArray && classArray[1].level} />
+          {!load && (
+            <ClassInput defaultValue={classArray && classArray[1].name} />
+          )}
+          {!load && (
+            <ClassValueInput
+              defaultValue={!load && classArray && classArray[1].level}
+            />
+          )}
         </li>
         <li>
-          <ClassInput defaultValue={classArray && classArray[2].name} />
-          <ClassValueInput defaultValue={classArray && classArray[2].level} />
+          {!load && (
+            <ClassInput defaultValue={classArray && classArray[2].name} />
+          )}
+          {!load && (
+            <ClassValueInput
+              defaultValue={!load && classArray && classArray[2].level}
+            />
+          )}
         </li>
         <li>
-          <ClassInput defaultValue={classArray && classArray[3].name} />
-          <ClassValueInput defaultValue={classArray && classArray[3].level} />
+          {!load && (
+            <ClassInput defaultValue={classArray && classArray[3].name} />
+          )}
+          {!load && (
+            <ClassValueInput
+              defaultValue={!load && classArray && classArray[3].level}
+            />
+          )}
         </li>
       </ul>
     </Container>
   )
+}
+
+CharClass.propTypes = {
+  classes: PropTypes.arrayOf(object).isRequired,
 }
