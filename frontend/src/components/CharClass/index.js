47aeaded33e2f40f8d11210ffbf8ef53bbaca0a5
Rogerio <rogerio.francis@gmail.com>
2020-05-26 16:12:46 -0400

    Teste single state

--- frontend/src/components/CharClass/index.js
+++ frontend/src/components/CharClass/index.js
@@ -3,41 +3,27 @@ import PropTypes from 'prop-types'
 
 import { Container, ClassInput, ClassValueInput } from './styles'
 
-export default function CharClass({ classes }) {
-  const [newClass, setNewClass] = useState({})
-  const [newClass2, setNewClass2] = useState({})
-  const [newClass3, setNewClass3] = useState({})
-  const [newClass4, setNewClass4] = useState({})
-
-  async function loadChass() {
-    setNewClass(classes && classes[0])
-    setNewClass2(classes && classes[1])
-    setNewClass3(classes && classes[2])
-    setNewClass4(classes && classes[3])
-  }
-
-  useEffect(() => {
-    loadChass()
-  }, []) // eslint-disable-line
+export default function CharClass({ classes, loading }) {
+  const [classArray, setClassArray] = useState(!loading && classes)
 
   return (
     <Container>
       <ul>
         <li>
-          <ClassInput defaultValue={newClass && newClass.name} />
-          <ClassValueInput defaultValue={newClass && newClass.level} />
+          <ClassInput defaultValue={classArray && classArray[0].name} />
+          <ClassValueInput defaultValue={classArray && classArray[0].level} />
         </li>
         <li>
-          <ClassInput defaultValue={newClass2 && newClass2.name} />
-          <ClassValueInput defaultValue={newClass2 && newClass2.level} />
+          <ClassInput defaultValue={classArray && classArray[1].name} />
+          <ClassValueInput defaultValue={classArray && classArray[1].level} />
         </li>
         <li>
-          <ClassInput defaultValue={newClass3 && newClass3.name} />
-          <ClassValueInput defaultValue={newClass3 && newClass3.level} />
+          <ClassInput defaultValue={classArray && classArray[2].name} />
+          <ClassValueInput defaultValue={classArray && classArray[2].level} />
         </li>
         <li>
-          <ClassInput defaultValue={newClass4 && newClass4.name} />
-          <ClassValueInput defaultValue={newClass4 && newClass4.level} />
+          <ClassInput defaultValue={classArray && classArray[3].name} />
+          <ClassValueInput defaultValue={classArray && classArray[3].level} />
         </li>
       </ul>
     </Container>
