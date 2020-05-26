96831925902bd079d1da0c8c9476df3ac0c47bd9
Rogerio <rogerio.francis@gmail.com>
2020-05-26 15:47:57 -0400

    Try  State Component

--- frontend/src/components/CharClass/index.js
+++ frontend/src/components/CharClass/index.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Container, ClassInput, ClassValueInput } from './styles'
 
 export default function CharClass({ classes, loading }) {
-  const len1 = !loading && classes && classes[0]
-  const len2 = !loading && classes && classes[1]
-  const len3 = !loading && classes && classes[2]
-  const len4 = !loading && classes && classes[3]
+  const [newClass, setNewClass] = useState()
+
+  async function loadChass() {
+    setNewClass(classes && classes[0])
+  }
+
+  useEffect(() => {
+    loadChass()
+  }, []) // eslint-disable-line
 
   return (
     <Container>
       <ul>
         <li>
-          <ClassInput defaultValue={len1.name} />
-          <ClassValueInput defaultValue={len1.level} />
+          <ClassInput defaultValue={newClass && newClass.name} />
+          <ClassValueInput defaultValue={newClass && newClass.level} />
         </li>
-        <li>
+        {/* <li>
           <ClassInput defaultValue={len2.name} />
           <ClassValueInput defaultValue={len2.level} />
         </li>
@@ -26,7 +31,7 @@ export default function CharClass({ classes, loading }) {
         <li>
           <ClassInput defaultValue={len4.name} />
           <ClassValueInput defaultValue={len4.level} />
-        </li>
+        </li> */}
       </ul>
     </Container>
   )
