83b2067f31265546f655d3dc98da1a3df3afffc1
Rogerio <rogerio.francis@gmail.com>
2020-05-26 15:14:48 -0400

    Li prefixed

--- frontend/src/components/CharClass/index.js
+++ frontend/src/components/CharClass/index.js
@@ -2,19 +2,32 @@ import React from 'react'
 
 import { Container, ClassInput, ClassValueInput } from './styles'
 
-export default function CharClass({ classes, loading }) {
+export default function CharClass({ classes }) {
+  const classLength = classes.length
+  const len1 = classes[0]
+  const len2 = classes[1]
+  const len3 = classes[2]
+  const len4 = classes[3]
+
   return (
     <Container>
       <ul>
-        {!loading &&
-          classes.length > 0 &&
-          classes.map((item, index) => (
-            // eslint-disable-next-line
-            <li key={index}>
-              <ClassInput defaultValue={item.name} />
-              <ClassValueInput defaultValue={item.level} />
-            </li>
-          ))}
+        <li>
+          <ClassInput defaultValue={len1.name} />
+          <ClassValueInput defaultValue={len1.level} />
+        </li>
+        <li>
+          <ClassInput defaultValue={len2.name} />
+          <ClassValueInput defaultValue={len2.level} />
+        </li>
+        <li>
+          <ClassInput defaultValue={len3.name} />
+          <ClassValueInput defaultValue={len3.level} />
+        </li>
+        <li>
+          <ClassInput defaultValue={len4.name} />
+          <ClassValueInput defaultValue={len4.level} />
+        </li>
       </ul>
     </Container>
   )
