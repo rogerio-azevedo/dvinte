01f9a4f1fbc6c9701766ec0ad3e05269489eccd8
Rogerio <rogerio.francis@gmail.com>
2020-05-26 21:25:57 -0400

    Stateless && loading

--- frontend/src/components/CharClass/index.js
+++ frontend/src/components/CharClass/index.js
@@ -3,26 +3,34 @@ import PropTypes, { object } from 'prop-types'
 
 import { Container, ClassInput, ClassValueInput } from './styles'
 
-export default function CharClass({ classes }) {
+export default function CharClass({ classes, loading }) {
   return (
     <Container>
       <ul>
         <li>
-          <ClassInput defaultValue={classes && classes[0].name} />
-          <ClassValueInput defaultValue={classes && classes[0].level} />
+          <ClassInput defaultValue={!loading && classes && classes[0].name} />
+          <ClassValueInput
+            defaultValue={!loading && classes && classes[0].level}
+          />
         </li>
         <li>
-          <ClassInput defaultValue={classes && classes[1].name} />
-          <ClassValueInput defaultValue={classes && classes[1].level} />
+          <ClassInput defaultValue={!loading && classes && classes[1].name} />
+          <ClassValueInput
+            defaultValue={!loading && classes && classes[1].level}
+          />
         </li>
         <li>
-          <ClassInput defaultValue={classes && classes[2].name} />
-          <ClassValueInput defaultValue={classes && classes[2].level} />
+          <ClassInput defaultValue={!loading && classes && classes[2].name} />
+          <ClassValueInput
+            defaultValue={!loading && classes && classes[2].level}
+          />
         </li>
         <li>
-          <ClassInput defaultValue={classes && classes[3].name} />
+          <ClassInput defaultValue={!loading && classes && classes[3].name} />
 
-          <ClassValueInput defaultValue={classes && classes[3].level} />
+          <ClassValueInput
+            defaultValue={!loading && classes && classes[3].level}
+          />
         </li>
       </ul>
     </Container>
@@ -31,4 +39,5 @@ export default function CharClass({ classes }) {
 
 CharClass.propTypes = {
   classes: PropTypes.arrayOf(object).isRequired,
+  loading: PropTypes.bool.isRequired,
 }
