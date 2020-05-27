d31e46e723a7cf9927692f6eb3bc65d7e5242fba
Rogerio <rogerio.francis@gmail.com>
2020-05-26 21:21:21 -0400

    Lest without state

--- frontend/src/components/CharClass/index.js
+++ frontend/src/components/CharClass/index.js
@@ -1,43 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import PropTypes, { object } from 'prop-types'
 
 import { Container, ClassInput, ClassValueInput } from './styles'
 
 export default function CharClass({ classes }) {
-  const [load, setLoad] = useState(true)
-  const [classArray, setClassArray] = useState([])
-
-  async function loadClass() {
-    setClassArray(classes)
-  }
-  useEffect(() => {
-    loadClass()
-    setLoad(false)
-  }, []) // eslint-disable-line
-
   return (
     <Container>
-      {!load && (
-        <ul>
-          <li>
-            <ClassInput defaultValue={classArray && classArray[0].name} />
-            <ClassValueInput defaultValue={classArray && classArray[0].level} />
-          </li>
-          <li>
-            <ClassInput defaultValue={classArray && classArray[1].name} />
-            <ClassValueInput defaultValue={classArray && classArray[1].level} />
-          </li>
-          <li>
-            <ClassInput defaultValue={classArray && classArray[2].name} />
-            <ClassValueInput defaultValue={classArray && classArray[2].level} />
-          </li>
-          <li>
-            <ClassInput defaultValue={classArray && classArray[3].name} />
+      <ul>
+        <li>
+          <ClassInput defaultValue={classes && classes[0].name} />
+          <ClassValueInput defaultValue={classes && classes[0].level} />
+        </li>
+        <li>
+          <ClassInput defaultValue={classes && classes[1].name} />
+          <ClassValueInput defaultValue={classes && classes[1].level} />
+        </li>
+        <li>
+          <ClassInput defaultValue={classes && classes[2].name} />
+          <ClassValueInput defaultValue={classes && classes[2].level} />
+        </li>
+        <li>
+          <ClassInput defaultValue={classes && classes[3].name} />
 
-            <ClassValueInput defaultValue={classArray && classArray[3].level} />
-          </li>
-        </ul>
-      )}
+          <ClassValueInput defaultValue={classes && classes[3].level} />
+        </li>
+      </ul>
     </Container>
   )
 }
