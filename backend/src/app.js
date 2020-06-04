fa0eb5ce3ad330c8ff0843e973d261c1f818486f
Rogerio <rogerio.francis@gmail.com>
2020-06-04 18:44:15 -0400

    Ajust Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ class App {
   }
 
   middlewares() {
-    this.server.use(cors())
+    this.server.use(cors('*'))
     this.server.use(express.json())
     this.server.use(
       '/portraits',
--- frontend/src/components/RenderMap/index.js
+++ frontend/src/components/RenderMap/index.js
@@ -1,95 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { Stage, Layer, Rect, Circle, Image, Transformer } from 'react-konva'
+import React, { useState } from 'react'
+import { Stage, Layer, Rect, Image } from 'react-konva'
 import useImage from 'use-image'
 
 const width = 80
 const height = 80
 
 const grid = [['white']]
-
-const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
-  const shapeRef = React.useRef()
-  const trRef = React.useRef()
-
-  useEffect(() => {
-    if (isSelected) {
-      // we need to attach transformer manually
-      trRef.current.setNode(shapeRef.current)
-      trRef.current.getLayer().batchDraw()
-    }
-  }, [isSelected])
-
-  return (
-    <>
-      <Rect
-        onClick={onSelect}
-        onTap={onSelect}
-        ref={shapeRef}
-        {...shapeProps}
-        draggable
-        onDragEnd={e => {
-          onChange({
-            ...shapeProps,
-            x: e.target.x(),
-            y: e.target.y(),
-          })
-        }}
-        onTransformEnd={e => {
-          // transformer is changing scale of the node
-          // and NOT its width or height
-          // but in the store we have only width and height
-          // to match the data better we will reset scale on transform end
-          const node = shapeRef.current
-          const scaleX = node.scaleX()
-          const scaleY = node.scaleY()
-
-          // we will reset it back
-          node.scaleX(1)
-          node.scaleY(1)
-          onChange({
-            ...shapeProps,
-            x: node.x(),
-            y: node.y(),
-            // set minimal value
-            width: Math.max(5, node.width() * scaleX),
-            height: Math.max(node.height() * scaleY),
-          })
-        }}
-      />
-      {isSelected && (
-        <Transformer
-          ref={trRef}
-          boundBoxFunc={(oldBox, newBox) => {
-            // limit resize
-            if (newBox.width < 5 || newBox.height < 5) {
-              return oldBox
-            }
-            return newBox
-          }}
-        />
-      )}
-    </>
-  )
-}
-
-const initialRectangles = [
-  {
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 100,
-    fill: 'red',
-    id: 'rect1',
-  },
-  {
-    x: 150,
-    y: 150,
-    width: 100,
-    height: 100,
-    fill: 'green',
-    id: 'rect2',
-  },
-]
 
 export default function RenderMap() {
   const [stagePos, setStagePos] = useState({ x: 0, y: 0 })
@@ -241,15 +157,46 @@ export default function RenderMap() {
     setTigerY(e.target.y())
   }
 
-  // rotate() {
-  //   const { angle } = this.state;
-  //   this.setState({
-  //     angle: angle + 90
-  //   });
-  //   this.imageNode.offsetX(this.imageNode.width() / 2);
-  //   this.imageNode.offsetY(this.imageNode.height() / 2);
-  //   this.imageNode.getLayer().draw();
-  // }
+  function rotate(character) {
+    switch (character) {
+      case 'worg':
+        setWorgAngle(worgAngle + 30)
+        break
+      case 'worg2':
+        setWorg2Angle(worg2Angle + 30)
+        break
+      case 'worgRider':
+        setWorgRiderAngle(worgRiderAngle + 30)
+        break
+      case 'tiger':
+        setTigerAngle(tigerAngle + 30)
+        break
+      case 'warrior':
+        setWarriorAngle(warriorAngle + 30)
+        break
+      case 'barbarian':
+        setBarbarianAngle(barbarianAngle + 30)
+        break
+      case 'rogue':
+        setRogueAngle(rogueAngle + 30)
+        break
+      case 'mage':
+        setMageAngle(mageAngle + 30)
+        break
+      case 'cleric':
+        setClericAngle(clericAngle + 30)
+        break
+      default:
+    }
+
+    // const { angle } = this.state
+    // this.setState({
+    //   angle: angle + 90,
+    // })
+    // this.imageNode.offsetX(this.imageNode.width() / 2);
+    // this.imageNode.offsetY(this.imageNode.height() / 2);
+    // this.imageNode.getLayer().draw();
+  }
 
   return (
     <Stage
@@ -273,9 +220,13 @@ export default function RenderMap() {
           width={130}
           height={130}
           offsetX={130 / 2}
+          offsetY={130 / 2}
           scaleX={1}
           rotation={worgAngle}
           onDragEnd={handleWorgDragEnd}
+          onClick={() => {
+            rotate('worg')
+          }}
         />
 
         <Image
@@ -286,9 +237,13 @@ export default function RenderMap() {
           width={130}
           height={130}
           offsetX={130 / 2}
+          offsetY={130 / 2}
           scaleX={1}
           rotation={worg2Angle}
           onDragEnd={handleWorg2DragEnd}
+          onClick={() => {
+            rotate('worg2')
+          }}
         />
 
         <Image
@@ -299,8 +254,12 @@ export default function RenderMap() {
           width={140}
           height={140}
           offsetX={140 / 2}
+          offsetY={140 / 2}
           rotation={worgRiderAngle}
           onDragEnd={handleWorgRiderDragEnd}
+          onClick={() => {
+            rotate('worgRider')
+          }}
         />
 
         <Image
@@ -311,8 +270,12 @@ export default function RenderMap() {
           width={140}
           height={140}
           offsetX={140 / 2}
+          offsetY={140 / 2}
           rotation={tigerAngle}
           onDragEnd={handleTigerDragEnd}
+          onClick={() => {
+            rotate('tiger')
+          }}
         />
 
         <Image
@@ -323,8 +286,12 @@ export default function RenderMap() {
           width={100}
           height={100}
           offsetX={100 / 2}
+          offsetY={100 / 2}
           rotation={rogueAngle}
           onDragEnd={handleRogueDragEnd}
+          onClick={() => {
+            rotate('rogue')
+          }}
         />
 
         <Image
@@ -332,11 +299,15 @@ export default function RenderMap() {
           x={barbarianX}
           y={barbarianY}
           image={barbarian}
-          width={110}
-          height={110}
-          offsetX={110 / 2}
+          width={120}
+          height={120}
+          offsetX={120 / 2}
+          offsetY={120 / 2}
           rotation={barbarianAngle}
           onDragEnd={handleBarbarianDragEnd}
+          onClick={() => {
+            rotate('barbarian')
+          }}
         />
 
         <Image
@@ -344,11 +315,15 @@ export default function RenderMap() {
           x={clericX}
           y={clericY}
           image={cleric}
-          width={90}
-          height={90}
-          offsetX={90 / 2}
+          width={95}
+          height={95}
+          offsetX={95 / 2}
+          offsetY={95 / 2}
           rotation={clericAngle}
           onDragEnd={handleClericDragEnd}
+          onClick={() => {
+            rotate('cleric')
+          }}
         />
 
         <Image
@@ -359,8 +334,12 @@ export default function RenderMap() {
           width={125}
           height={125}
           offsetX={125 / 2}
+          offsetY={125 / 2}
           rotation={warriorAngle}
           onDragEnd={handleWarriorDragEnd}
+          onClick={() => {
+            rotate('warrior')
+          }}
         />
 
         <Image
@@ -371,8 +350,12 @@ export default function RenderMap() {
           width={100}
           height={100}
           offsetX={100 / 2}
+          offsetY={100 / 2}
           rotation={mageAngle}
           onDragEnd={handleMageDragEnd}
+          onClick={() => {
+            rotate('mage')
+          }}
         />
       </Layer>
     </Stage>
