d36804e86f9dd00d5da48436b90224a2d3f76170
Rogerio Azevedo <rogerio.francis@gmail.com>
2020-09-26 17:52:43 -0400

    Ajust Sizes

--- backend/src/util/getSize.js
+++ backend/src/util/getSize.js
@@ -3,13 +3,31 @@ export default function getSize(size) {
 
   switch (size) {
     case 1:
-      text = 'PEQUENO'
+      text = 'MINÚSCULO'
       break
     case 2:
-      text = 'MÉDIO'
+      text = 'DIMINUTO'
       break
     case 3:
+      text = 'MIÚDO'
+      break
+    case 4:
+      text = 'PEQUENO'
+      break
+    case 5:
+      text = 'MÉDIO'
+      break
+    case 6:
       text = 'GRANDE'
+      break
+    case 7:
+      text = 'ENORME'
+      break
+    case 8:
+      text = 'IMENSO'
+      break
+    case 9:
+      text = 'COLOSSAL'
       break
     default:
   }
